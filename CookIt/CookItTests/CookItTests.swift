0513348adf754f4df2a1ee61147e3f39a4eda9e7
Daniel Torres <danstorre@gmail.com>
2017-03-23 00:20:44 -0300

    project created

---
+++ CookIt/CookItTests/CookItTests.swift
@@ -0,0 +1,36 @@
+//
+//  CookItTests.swift
+//  CookItTests
+//
+//  Created by Daniel Torres on 3/23/17.
+//  Copyright © 2017 Danieltorres. All rights reserved.
+//
+
+import XCTest
+@testable import CookIt
+
+class CookItTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
