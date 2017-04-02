f092fd42f10b097d5ac9df3ee52c5497280227e0
Daniel Torres <danstorre@gmail.com>
2017-04-02 15:32:58 -0300

    adding credentials file

---
+++ CookIt/CookIt/Credentials_.swift
@@ -0,0 +1,18 @@
+//
+//  Credentials.swift
+//  OntheMap_DanielTorres
+//
+//  Created by Daniel Torres on 12/17/16.
+//  Copyright © 2016 Daniel Torres. All rights reserved.
+//
+
+
+import UIKit
+
+struct Credentials {
+    
+    static let mashapeKey = "0Zo2zuWOV8mshOs3Jcn8iY8wq3T6p10hYhijsnKPFrCE2DOwtH"
+    
+    
+    
+}
